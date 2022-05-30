471d174ffd593bb3163c1c97816cdf951ea49795
kazunishikawa <kazuhiro891@gmail.com>
2022-05-31 08:49:37 +0900

    first commit

---
+++ urawa/src/main/java/jp/saitama/saitama/urawa/UrawaApplication.java
@@ -0,0 +1,13 @@
+package jp.saitama.saitama.urawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrawaApplication.class, args);
+	}
+
+}
---
+++ urawa/src/test/java/jp/saitama/saitama/urawa/UrawaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.saitama.saitama.urawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
